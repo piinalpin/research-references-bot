4efec64d73a398a0ad3a8c497b395e155955273b
Alvinditya Saputra <alvindityas@gmail.com>
2019-05-19 20:29:35 +0700

    add line bot

--- server.py
+++ server.py
@@ -1,53 +1,97 @@
 from bot import ChatBot
 from search_engine import Engine
 from tensor_flow import TensorFlow
-
-update_id = None
-resp = TensorFlow(intents="docs/intents.json")
-
-
-def make_reply(msg):
-    txt = None
-    if msg is not None:
-        txt = resp.response(msg)
-    return txt
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
 
 
-while True:
-    print("....")
-    bot = ChatBot("config.cfg")
-    updates = bot.get_updates(offset=update_id)
-    updates = updates["result"]
-    if updates:
-        for item in updates:
-            update_id = item["update_id"]
-            try:
-                if "message" in item:
-                    message = item["message"]["text"]
-                elif "sticker" in item:
-                    message = "Ok"
-                else:
-                    message = item["edited_message"]["text"]
-            except:
-                message = None
-            if message is None:
-                continue
-            try:
-                from_ = item["message"]["from"]["id"]
-            except:
-                from_ = item["edited_message"]["from"]["id"]
-            text = message.partition("tentang")
-            if "tentang" not in text:
-                reply = make_reply(message)
-                bot.send_message(reply, from_)
-            else:
-                query = Engine(query=text[2].lstrip())
-                dictionary = query.get_scores()
-                for i in range(len(dictionary["author"])):
-                    author = list(dictionary["author"])[i].lstrip()
-                    title = list(dictionary["title"])[i].lstrip()
-                    url = list(dictionary["url"])[i].lstrip()
-                    year = list(dictionary["year"])[i]
-                    score = float(list(dictionary["score"])[i])
-                    msg = "*Author:* {}%0D%0A*Title:* {}%0D%0A*Url:* {}%0D%0A*Year:* {}".format(author, title, url, year)
-                    bot.send_message(msg, from_)
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('PmJtqckdfsQ21NL+kIgVHNVeraY4MQ/WTY+ysR8ZDPWRFCs8+LseFMWR1iRSLic0o0q0pUvnaEchYl1phF75ZYoNaVoEnt+z4wEaEOgI7+pEsGtRsiAS054smc17eLN2VV3fZ27VRVDjQfWugdKR6AdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('44ea69c0b9b62849571e363d93e53127')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # Get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # Get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # Handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    """ Here's all the messages will be handled and processed by the program """
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+# update_id = None
+# resp = TensorFlow(intents="docs/intents.json")
+
+
+# def make_reply(msg):
+#     txt = None
+#     if msg is not None:
+#         txt = resp.response(msg)
+#     return txt
+#
+#
+# while True:
+#     print("....")
+#     bot = ChatBot("config.cfg")
+#     updates = bot.get_updates(offset=update_id)
+#     updates = updates["result"]
+#     if updates:
+#         for item in updates:
+#             update_id = item["update_id"]
+#             try:
+#                 if "message" in item:
+#                     message = item["message"]["text"]
+#                 elif "sticker" in item:
+#                     message = "Ok"
+#                 else:
+#                     message = item["edited_message"]["text"]
+#             except:
+#                 message = None
+#             if message is None:
+#                 continue
+#             try:
+#                 from_ = item["message"]["from"]["id"]
+#             except:
+#                 from_ = item["edited_message"]["from"]["id"]
+#             text = message.partition("tentang")
+#             if "tentang" not in text:
+#                 reply = make_reply(message)
+#                 bot.send_message(reply, from_)
+#             else:
+#                 query = Engine(query=text[2].lstrip())
+#                 dictionary = query.get_scores()
+#                 for i in range(len(dictionary["author"])):
+#                     author = list(dictionary["author"])[i].lstrip()
+#                     title = list(dictionary["title"])[i].lstrip()
+#                     url = list(dictionary["url"])[i].lstrip()
+#                     year = list(dictionary["year"])[i]
+#                     score = float(list(dictionary["score"])[i])
+#                     msg = "*Author:* {}%0D%0A*Title:* {}%0D%0A*Url:* {}%0D%0A*Year:* {}".format(author, title, url, year)
+#                     bot.send_message(msg, from_)
